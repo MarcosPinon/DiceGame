c5de16154de14111cd31f51ac6c6cde204809da2
Marcos Antonio <94643613+MarcosPinon@users.noreply.github.com>
2024-03-05 16:20:31 -0300

    updated js and added a button

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var randomNumber1 = Math.floor(Math.random()* 6) + 1;
+ /* var randomNumber1 = Math.floor(Math.random()* 6) + 1;
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 
 
@@ -12,4 +12,25 @@ if(randomNumber1 > randomNumber2){
     document.querySelector("h1").textContent="Player 2 Wins!"
 } else if (randomNumber1 === randomNumber2){
     document.querySelector("h1").textContent="Draw!"
+} */
+
+
+document.getElementById("dicebtn").addEventListener("click",diceRoll);
+
+function diceRoll(){
+    var randomNumber1 = Math.floor(Math.random()* 6) + 1;
+    var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+    document.querySelector(".img1").src=`images/dice${randomNumber1}.png`
+
+document.querySelector(".img2").src=`images/dice${randomNumber2}.png`
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").textContent="Player 1 Wins!"
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").textContent="Player 2 Wins!"
+} else if (randomNumber1 === randomNumber2){
+    document.querySelector("h1").textContent="Draw!"
+}
+    
 }
